ed811f7519bf0bb21f71dede09bff842ff6d74a0
devByBjorn <devbybjorn@gmail.com>
2020-03-20 08:42:52 +0100

    indexPage

--- src/components/pages/AboutParagraphs.js
+++ src/components/pages/AboutParagraphs.js
@@ -12,13 +12,13 @@ export const backstory = [backstoryOne, backstoryTwo]
 // WHY FRONT-END
 const whyFrontEndOne = "I was looking for a field where I could fuel my creativity and be able to constantly develop - a field that wasn’t necessarily bound to one sector. Front-End Development fitted that description pretty darn well."
 
-const whyFrontEndTwo = "I would lie if I said I never questioned my choice, the leap. After three hours down trying to solve a bug that turns out being a spelling mistake in the entry path in the webpack config file, who wouldn’t? But in the end, when a problem is solved, especially a problem that seemed near impossible, it’s all worth it."
+const whyFrontEndTwo = "I would lie if I said I never questioned my choice, the leap. After three hours down trying to solve a bug that turns out being a spelling mistake in the entry path in the webpack config file, or when I deployed this page and realized I've styled the whole page width 175% zoom in the browser, of course I can questione my choice. But in the end, when a problem is solved, especially a problem that seemed near impossible, it’s all worth it."
 
 const whyFrontEndThree = "A big part of the charm with development, for me, is dealing with contrasting feelings, going from feeling like the dumbest person on the planet one day to a genius the next."
 
 export const whyFrontEnd = [whyFrontEndOne, whyFrontEndTwo, whyFrontEndThree]
 
-const whatIBringOne = "I spent about 10 years on customer focus-, business- and sales driven work related tasks. Four years in banking. The last three years was in a Nordic(sub-international) organization. An organization where I had daily contact and collaboration with people from all over the bank. "
+const whatIBringOne = "I've been working on customer focus-, business- and sales driven related tasks for about 10 years. Four years of those in banking. The last three years was in a Nordic(sub-international) organization. An organization where I had daily contact and collaboration with people from all over the bank. "
 
 const whatIBringTwo = "This environment had a great impact on my ability to communicate in a free and clear manner, with people of various backgrounds, ages and positions. From markets salespeople, retail bankers, private bankers, investment bankers to IT maintenance, developers and customer supporters, and many more. To communicate well I needed to understand others and what perspective they came from. An understanding I hold high today"
 
