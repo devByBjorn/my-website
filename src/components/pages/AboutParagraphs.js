fe2d4b47d4a9f82065209d06fdc1a1d49dfef427
devByBjorn <devbybjorn@gmail.com>
2020-04-06 10:51:15 +0200

    about and firstpage paragrapghs

--- src/components/pages/AboutParagraphs.js
+++ src/components/pages/AboutParagraphs.js
@@ -8,16 +8,16 @@ export const backstory = [backstoryOne, backstoryTwo]
 // WHY FRONT-END
 const whyFrontEndOne = "I was looking for a field where I could fuel my creativity and be able to constantly develop - a field that wasn’t necessarily bound to one sector. Front-End Development fitted that description pretty darn well."
 
-const whyFrontEndTwo = "I would lie if I said I never questioned my choice, the leap. After three hours down trying to solve a bug that turns out being a spelling mistake in the entry path in the webpack config file, yes, I questioned my choice. Or when I after deployment of a site realized I've styled the whole project width 175% zoom in the browser, yes, I questioned my choice. But in the end, when a problem is solved, especially a problem that seemed near impossible, it’s all worth it."
+const whyFrontEndTwo = "I would lie if I said I never questioned my choice, the leap. After three hours down trying to solve a bug that turns out being a spelling mistake in the entry path in the webpack config file, yes, I questioned my choice. Or when, after deployment of a site, I realized I've styled the whole project with 175% zoom in the browser, yes, I questioned my choice. But in the end, when a problem is solved, especially a problem that seemed near impossible, it’s all worth it."
 
-const whyFrontEndThree = "A big part of the charm with development, for me, is dealing with contrasting feelings. One day feeling like the dumbest person on the planet and the next a real genius."
+const whyFrontEndThree = "A big part of the charm with development, for me, is dealing with contrasting feelings. One day feeling like the dumbest person on the planet and the next a real genius. I love the twist and turns."
 
 export const whyFrontEnd = [whyFrontEndOne, whyFrontEndTwo, whyFrontEndThree]
 
 // WHAT I BRING TO THE TABLE
-const whatIBringOne = "I've been working with customer focus-, business- and sales-driven related tasks for about 10 years. Four years of those in banking. The last three years was in a Nordic, sub-international organization. An organization where I had daily contact and collaboration with people from all over the bank."
+const whatIBringOne = "I've been working with customer focus-, business- and sales-driven related tasks for about 10 years. Four years of those in banking. The last three years was in a Nordic, sub-international organization. An organization where I had daily contact and collaboration with people all over the bank."
 
-const whatIBringTwo = "This environment had a great impact on my ability to communicate in a free and clear manner, with people of various backgrounds, ages and positions. From markets salespeople, retail bankers, private bankers, investment bankers to IT maintenance, developers and customer supporters. To communicate well I needed to understand others and what perspective they came from, an understanding I hold high today"
+const whatIBringTwo = "This environment had a great impact on my ability to communicate in a free and clear manner, with people of various backgrounds, ages and positions. From markets salespeople, retail bankers, private bankers, investment bankers to IT maintenance, developers and customer supporters. To communicate well with all these people I needed to understand others and what perspective they came from, a lesson and an understanding I hold high today."
 
 const whatIBringThree = "The above skills, communication and seeing things from others perspective, together with a pretty good problem-solving skill, a willingness to learn and contribute, and a goal to always stay humble, that is what I bring to the table."
 
