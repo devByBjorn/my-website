42d5d0910e519b170a16fc20888d81ea33cea985
devByBjorn <devbybjorn@gmail.com>
2020-06-06 15:44:16 +0200

    project descriptions

--- src/components/pages/PortfolioPage.js
+++ src/components/pages/PortfolioPage.js
@@ -20,7 +20,7 @@ const PortfolioPage = ({ location }) => (
         techHeading="Technologies"
         techContent="React | Styled Components | Webpack | Babel | REST"
         aboutHeading="About"
-        aboutContent="Challange from frontendmentor.io. Learned alot by working with REST Countries API, got a hang of Styled Components and found out how to create a dark/light switch with Hooks."
+        aboutContent="Challenge from frontendmentor.io, where the idea is to mimic real workflow. I was given a description of what functionality the app should have and a set design to follow when styling. Learned a lot working with a REST API, got a hang of Styled Components and how to create a dark/light switch with Hooks. "
         projectUrl="https://search-world.herokuapp.com/"
         repoUrl="https://github.com/devByBjorn/where-in-the-world"
       />
@@ -40,7 +40,7 @@ const PortfolioPage = ({ location }) => (
         techHeading="Technologies"
         techContent="HTML | CSS | JavaScript | Webpack | Babel"
         aboutHeading="About"
-        aboutContent="A website focused on delivering an easy overview of time, market hours and opening info of various stock markets around the world."
+        aboutContent="A website focused on delivering an easy overview of time, market hours and opening info of various stock markets around the world. Got a real good understanding of wirking with time in JavaScript with the third pary library moment.js"
         projectUrl="https://www.marketsintime.com/"
         repoUrl="https://github.com/devByBjorn/markets_time"
       />
@@ -50,7 +50,7 @@ const PortfolioPage = ({ location }) => (
         techHeading="Technologies"
         techContent="HTML | Sass | JavaScript | Webpack | Babel"
         aboutHeading="About"
-        aboutContent="A website tribute to the movie Pulp Fiction. Generates quotes from the film and lets you save your favorites. If you feel creative you can write, save and edit your own quotes as well."
+        aboutContent="A website tribute to the movie Pulp Fiction. Generates quotes from the film and lets you save your favorites. If you feel creative you can write, save and edit your own quotes as well. First app I made with real interactivity, letting the user create and save their own data(qoutes) with the help of local storage."
         projectUrl="https://sharp-engelbart-11818a.netlify.com/"
         repoUrl="https://github.com/devByBjorn/the_PFQT"
       />
@@ -60,7 +60,7 @@ const PortfolioPage = ({ location }) => (
         techHeading="Technologies"
         techContent="HTML | Sass | JavaScript | Webpack | Babel"
         aboutHeading="About"
-        aboutContent="HTML and CSS challenge from frontendmentor.io. I chose to do the functionality with JavaScript and added a minor feature; darken the active subscription period on toggle."
+        aboutContent="HTML and CSS challenge from frontendmentor.io where the idea is to mimic real workflow. I was given a description of what functionality the app should have and a set design to follow when styling. Forst project I did using CSS preprocesser Sass"
         projectUrl="https://xenodochial-bartik-7585b3.netlify.com"
         repoUrl="https://github.com/devByBjorn/price_component"
       />
@@ -70,7 +70,7 @@ const PortfolioPage = ({ location }) => (
         techHeading="Technologies"
         techContent="HTML | CSS | JavaScript"
         aboutHeading="About"
-        aboutContent="The classic Hangman game made into a geographical based guessing game; the user gets to guess capitals around the world"
+        aboutContent="The classic Hangman game made into a geographical based guessing game; the user gets to guess capitals around the world. Here I got to dig deep into async fetching, promises and working with a REST API."
         projectUrl="https://romantic-elion-2f9c7c.netlify.com/"
         repoUrl="https://github.com/devByBjorn/Guess_the_capital"
       />
